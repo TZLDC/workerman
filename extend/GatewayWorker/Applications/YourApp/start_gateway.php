814973cae742488ac6bc157271096153f3846624
LDC <859314714@qq.com>
2018-08-23 16:15:20 +0800

    222

--- extend/GatewayWorker/Applications/YourApp/start_gateway.php
+++ extend/GatewayWorker/Applications/YourApp/start_gateway.php
@@ -21,18 +21,18 @@ use \Workerman\Autoloader;
 require_once __DIR__ . '/../../vendor/autoload.php';
 
 // gateway 进程，这里使用Text协议，可以用telnet测试
-$gateway = new Gateway("Websocket://192.168.0.184:8282");
+$gateway = new Gateway("Websocket://0.0.0.0:8282");
 // gateway名称，status方便查看
 $gateway->name = 'YourAppGateway';
 // gateway进程数
 $gateway->count = 4;
 // 本机ip，分布式部署时使用内网ip
-$gateway->lanIp = '192.168.0.184';
+$gateway->lanIp = '0.0.0.0';
 // 内部通讯起始端口，假如$gateway->count=4，起始端口为4000
 // 则一般会使用4000 4001 4002 4003 4个端口作为内部通讯端口 
 $gateway->startPort = 2900;
 // 服务注册地址
-$gateway->registerAddress = '192.168.0.184:1238';
+$gateway->registerAddress = '0.0.0.0:1238';
 
 // 心跳间隔
 $gateway->pingInterval = 10;
